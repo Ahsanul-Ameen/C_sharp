f62cdec78b9dce4c4f71d9aebaf55843c0051beb
Ahsanul-Ameen <ahsanulsabit7@gmail.com>
2021-04-20 04:40:24 +0600

    Stroustups book added

--- OOP_C++/coursera-cs400/cpp-memory/main.cpp
+++ OOP_C++/coursera-cs400/cpp-memory/main.cpp
@@ -8,14 +8,19 @@
 #include <iostream>
 
 int main() {
+
+  int * p;
+
   int num = 7;
   std::cout << " num: " <<  num << std::endl;
   std::cout << "&num: " << &num << std::endl;
 
-  int *p = &num;
+  p = &num;
   std::cout << " p: " <<  p << std::endl;
   std::cout << "&p: " << &p << std::endl;
   std::cout << "*p: " << *p << std::endl;
+
+  std::cout << "&p - &num = " << (long long)(&p) - (long long)(&num) << std::endl;
 
   *p = 42;
   std::cout << "*p changed to 42" << std::endl; 
