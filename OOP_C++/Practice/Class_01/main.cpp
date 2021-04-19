bca6f0cb4c648641f0539a5a2e0df325ad6136aa
Ahsanul-Ameen <ahsanulsabit7@gmail.com>
2021-04-19 14:53:04 +0600

    Some Books shuffled

---
+++ OOP_C++/Practice/Class_01/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h> // c++
+using namespace std;
+
+
+int main() {
+
+    int x = INT_MAX; 
+    cout << x << endl;
+
+    x = INT_MIN;
+    cout << x << endl;
+    
+  
+  return 0;
+}
