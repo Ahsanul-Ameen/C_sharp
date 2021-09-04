5b5342cd62d8e70b53fb87476505bbc4322d1431
Ahsanul_Ameen <ahsanulsabit7@gmail.com>
2021-09-04 19:36:23 +0600

    remove ICMP sniffer and spoofer c programs

--- CODE/ping.sh
+++
@@ -1,5 +0,0 @@
-gcc -w spoof_icmp.c -o icmp_ping_spoof
-sudo ./icmp_ping_spoof "$1" "$2" "$3"
-# source ip
-# dest ip
-# npackets
--- CODE/sniff.sh
+++
@@ -1,8 +0,0 @@
-# gcc sniff-raw.c -o sniffer
-# sudo ./sniffer
-
-gcc sniff-pcap.c -o sniffer -lpcap
-sudo ./sniffer "$1"
-
-# gcc sniff-raw.c -o sniffer
-# sudo ./sniffer
