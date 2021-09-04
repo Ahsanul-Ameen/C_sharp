04e543ebe258c408b4aae2fbe406c25067cb734b
Ahsanul_Ameen <ahsanulsabit7@gmail.com>
2021-09-04 19:26:46 +0600

    Update recursion17.c

--- OOP_C++/Practice/Class_08/recursion17.c
+++ OOP_C++/Practice/Class_08/recursion17.c
@@ -6,11 +6,11 @@
 /*
 Problem 17:
 
-Write a recursive program that determines whether a given sentence is palindromic or not just considering the 
+Write a recursive program that determines whether a given sentence is palindromic or not just considering the
 alpha-numeric characters ('a'-'z'), ('A'-'Z'), ('0'-'9').
 
 Input:
-madam, i'm adam
+madam, i'i ,madam
 hulala
 Output:
 palindromic
@@ -24,7 +24,7 @@ int main(void) {
 	int isPalindromic(char *str, int l, int r) {
 		if (l >= r) return 1;
 
-		if (str[l] != str[r]) 
+		if (str[l] != str[r])
 			return 0;
 		return isPalindromic(str, l + 1, r - 1);
 	}
@@ -33,7 +33,7 @@ int main(void) {
 	// follow this link : https://stackoverflow.com/questions/6282198/reading-string-from-input-with-space-character
 
 	scanf("%[^\n]s", str); // \n just sets the delimiter for the scanned string.
-	
+
 	int status = isPalindromic(str, 0, strlen(str) - 1);
 
 	printf("%s\n", status ? "palindromic" : "not palindromic");
@@ -41,5 +41,5 @@ int main(void) {
 	return 0;
 }
 
- 
+
 // gcc -o recursion17 recursion17.c && ./recursion17
